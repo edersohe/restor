851d0c061831d95a436d1916efd7feeb370a3a85
Eder Sosa <eder.sohe@gmail.com>
2014-03-31 23:57:03 -0600

    pep8 fixes

--- restor.py
+++ restor.py
@@ -4,87 +4,83 @@ import tornado.web
 
 def action_routes(prefix, id_regex='[0-9a-f]+'):
     _id = "(?P<_id>%s)" % id_regex
-    _arg1 =  "(?:/(?P<_arg1>edit|delete))?"
+    _arg1 = "(?:/(?P<_arg1>edit|delete))?"
     _arg2 = "(?P<_arg2>new)"
     route = "(?:/(?:(?:%s%s)|%s))?" % (_id, _arg1, _arg2)
     return r'' + prefix + route
 
+
 class ActionsHandler(tornado.web.RequestHandler):
     def get(self, *args, _id=None, _arg1=None, _arg2=None, **kwargs):
-    	print(self.request.path)
-    	print (_id, _arg1, _arg2)
+        print(self.request.path)
+        print(_id, _arg1, _arg2)
 
-    	if not _id and not _arg2 and not _arg1:
-    		self.index(*args, **kwargs)
-    	elif _id and not _arg2 and not _arg1:
-    		self.show(_id, *args, **kwargs)
-    	elif not _id and not _arg1 and _arg2 == 'new':
-    		self.new(*args, **kwargs)
-    	elif _id and not _arg2 and _arg1 == 'edit':
-    		self.edit(_id, *args, **kwargs)
-    	else:
-    		raise tornado.web.HTTPError(404)
+        if not _id and not _arg2 and not _arg1:
+            self.index(*args, **kwargs)
+        elif _id and not _arg2 and not _arg1:
+            self.show(_id, *args, **kwargs)
+        elif not _id and not _arg1 and _arg2 == 'new':
+            self.new(*args, **kwargs)
+        elif _id and not _arg2 and _arg1 == 'edit':
+            self.edit(_id, *args, **kwargs)
+        else:
+            raise tornado.web.HTTPError(404)
 
     def post(self, *args, _id=None, _arg1=None, _arg2=None, **kwargs):
-    	if not _id and not _arg1 and not _arg2:
-    		self.create(*args, **kwargs)
-    	elif _id and not _arg1 and not _arg2:
-    		self.update(_id, *args, **kwargs)
-    	elif _id and not _arg2 and _arg1 == 'delete':
-    		self.destroy(_id, *args, **kwargs)
-    	else:
-    		raise tornado.web.HTTPError(404)
-    	
+        if not _id and not _arg1 and not _arg2:
+            self.create(*args, **kwargs)
+        elif _id and not _arg1 and not _arg2:
+            self.update(_id, *args, **kwargs)
+        elif _id and not _arg2 and _arg1 == 'delete':
+            self.destroy(_id, *args, **kwargs)
+        else:
+            raise tornado.web.HTTPError(404)
+
     def put(self, *args, _id=None, _arg1=None, _arg2=None, **kwargs):
-    	if _id and not _arg1 and not _arg2:
-    		self.update(_id, *args, **kwargs)
-    	else:
-    		raise tornado.web.HTTPError(404)
-    	
+        if _id and not _arg1 and not _arg2:
+            self.update(_id, *args, **kwargs)
+        else:
+            raise tornado.web.HTTPError(404)
 
     def delete(self, *args, _id=None, _arg1=None, _arg2=None, **kwargs):
-    	if _id and not _arg1 and not _arg2:
-    		self.destroy(_id, *args, **kwargs)
-    	else:
-    		raise tornado.web.HTTPError(404)
-    	
+        if _id and not _arg1 and not _arg2:
+            self.destroy(_id, *args, **kwargs)
+        else:
+            raise tornado.web.HTTPError(404)
 
     def index(self, *args, **kwargs):
-    	print("index")
-    	raise tornado.web.HTTPError(405)
+        print("index")
+        raise tornado.web.HTTPError(405)
 
     def new(self, *args, **kwargs):
-    	print("new")
-    	raise tornado.web.HTTPError(405)
+        print("new")
+        raise tornado.web.HTTPError(405)
 
     def create(self, *args, **kwargs):
-    	print("create")
-    	raise tornado.web.HTTPError(405)
+        print("create")
+        raise tornado.web.HTTPError(405)
 
     def show(self, _id, *args, **kwargs):
-    	print("show", _id)
-    	raise tornado.web.HTTPError(405)
+        print("show", _id)
+        raise tornado.web.HTTPError(405)
 
     def edit(self, _id, *args, **kwargs):
-    	print("edit", _id)
-    	raise tornado.web.HTTPError(405)
+        print("edit", _id)
+        raise tornado.web.HTTPError(405)
 
     def update(self, _id, *args, **kwargs):
-    	print("update", _id)
-    	raise tornado.web.HTTPError(405)
+        print("update", _id)
+        raise tornado.web.HTTPError(405)
 
     def destroy(self, _id, *args, **kwargs):
-    	print("destroy", _id)
-    	raise tornado.web.HTTPError(405)
-
-
-
+        print("destroy", _id)
+        raise tornado.web.HTTPError(405)
 
 
 application = tornado.web.Application([
     (action_routes('/animal'), ActionsHandler)
-    # (r"/?(?:(?:(?P<_id>[0-9a-f]+)(?:/(?P<_arg1>edit|delete))?)|(?P<_arg2>new))?", ResTorHandler),
 ])
+
 
 if __name__ == "__main__":
     application.listen(8888)
